8c12265fc78af3c9fa1ca4af4cb4dcb48231d65d
pawcza <pawel.czarniecki@eu.spectrumbrands.com>
2018-12-18 15:56:39 +0000

    Working on serviceWorker * Added workbox plugin to generate serviceWorker * Added rel=noopener to some links in the footer

---
+++ precache-manifest.44021bc42746050bf8bf8216a4ae3506.js
@@ -0,0 +1,24 @@
+self.__precacheManifest = [
+  {
+    "revision": "75df2582f66f2fa086be",
+    "url": "runtime.bundle.js"
+  },
+  {
+    "revision": "3ac785acc0d247ae88bd3e7494cdfa9d",
+    "url": "index.html"
+  },
+  {
+    "revision": "fabff6a1aa6b1a6dcfd65ad9527e65f5",
+    "url": "app.bundle.js.gz"
+  },
+  {
+    "revision": "94f9551e54bcd522c9ee",
+    "url": "app.bundle.js"
+  },
+  {
+    "url": "0.d21f0da218cd19ba779a.css.gz"
+  },
+  {
+    "url": "0.d21f0da218cd19ba779a.css"
+  }
+];
