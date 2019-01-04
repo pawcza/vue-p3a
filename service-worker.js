c7fdaacf4d547315c4f527c58ea80c87d8443eb8
pawcza <pawel.czarniecki@eu.spectrumbrands.com>
2019-01-04 14:42:31 +0000

    Small fix & G suite * Added google page verification * Removed padding between boxes on desktop * Added bigger border on projects on desktop * Made the project description bigger

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.112b0e6cc21dc6497ac78fb75ded9df6.js"
+  "precache-manifest.69e61465fee9888a6becf563012c97b3.js"
 );
 
 /**
