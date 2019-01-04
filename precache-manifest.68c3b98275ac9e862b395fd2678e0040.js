14608985b08501db747160b81aac6c96fedfced4
pawcza <pawel.czarniecki@eu.spectrumbrands.com>
2019-01-04 15:11:41 +0000

    Small fix & G suite * Added missing styles

---
+++ precache-manifest.68c3b98275ac9e862b395fd2678e0040.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = [
+  {
+    "revision": "75df2582f66f2fa086be",
+    "url": "runtime.bundle.js"
+  },
+  {
+    "url": "manifest.303adae88a8b142a8e6f7a23b3c114bc.json"
+  },
+  {
+    "revision": "b12fe489e4a43d1a0976fd350f95a97c",
+    "url": "index.html"
+  },
+  {
+    "revision": "eb075916b7ae852c51d00c8140925470",
+    "url": "app.bundle.js.gz"
+  },
+  {
+    "revision": "f65db6ee83808c330988",
+    "url": "app.bundle.js"
+  },
+  {
+    "url": "0.ce20123c1372e73bd5ff.css.gz"
+  },
+  {
+    "url": "0.ce20123c1372e73bd5ff.css"
+  }
+];
