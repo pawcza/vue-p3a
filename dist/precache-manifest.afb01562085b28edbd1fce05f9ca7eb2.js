ffd7312ab7ee5e7b9271a4c0d34f1d1e5e7fa055
Pawel Czarniecki <pczarniecki@genesis.com.mt>
2019-09-11 17:43:48 +0200

    Build

---
+++ dist/precache-manifest.afb01562085b28edbd1fce05f9ca7eb2.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = [
+  {
+    "revision": "75df2582f66f2fa086be",
+    "url": "runtime.bundle.js"
+  },
+  {
+    "url": "manifest.303adae88a8b142a8e6f7a23b3c114bc.json"
+  },
+  {
+    "revision": "c211b8c10d82d0157ce2d1b3140b24a6",
+    "url": "index.html"
+  },
+  {
+    "revision": "20bd2ee371572d56065077a9979352a9",
+    "url": "app.bundle.js.gz"
+  },
+  {
+    "revision": "3653779cdffe7c027841",
+    "url": "app.bundle.js"
+  },
+  {
+    "url": "0.1f3207f2cfe62410412d.css.gz"
+  },
+  {
+    "url": "0.1f3207f2cfe62410412d.css"
+  }
+];
